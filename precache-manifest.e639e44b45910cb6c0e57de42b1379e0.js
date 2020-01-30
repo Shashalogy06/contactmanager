eaeab551c01c5967e3a8953cc0136267cc37d6cf
Shashi Priya <shashi.priya06@gmail.com>
2020-01-30 18:09:46 +0530

    Updates

---
+++ precache-manifest.e639e44b45910cb6c0e57de42b1379e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09531b1bede4d578531ad0cedf49593b",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "ba11efed6dd1f412ea30",
+    "url": "/contactmanager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "530273e45dd2380c8065",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "ba11efed6dd1f412ea30",
+    "url": "/contactmanager/static/js/2.9ba5a07e.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contactmanager/static/js/2.9ba5a07e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "530273e45dd2380c8065",
+    "url": "/contactmanager/static/js/main.12af5796.chunk.js"
+  },
+  {
+    "revision": "d86057960233a7428d99",
+    "url": "/contactmanager/static/js/runtime-main.2ce66dc9.js"
+  }
+]);
